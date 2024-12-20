5bbb779f48df1733846dd870ba52baf86fa8eee9
Imperador Napolitano <luna.noronha@unesp.br>
2024-12-19 21:24:54 -0300

    feat(routes): adicionei as routes e organizei os arquivos em pastas. BREAKING CHANGE: já, desdo início é bom separar/organizar os arquivos em pastas; as routes estão funcionando, pelo menos na minha máquina.

---
+++ projeto-rjm/src/pages/App.jsx
@@ -0,0 +1,16 @@
+import "./style/App.css"
+
+function App() {
+  return (
+      <>
+        <h1>Welcome!</h1>
+        <br/>
+        <a href='/login'><button>Login</button></a>
+        <br/>
+        <br/>
+        <a href='/registro'><button>Registro</button></a>
+      </>
+  )
+}
+
+export default App
--- projeto-rjm/src/pages/Login.jsx
+++ projeto-rjm/src/pages/Login.jsx
@@ -1,14 +1,21 @@
 const Login = ()=>{
     return (
         <form>
+            <a href="/">Voltar</a>
+            <br/>
+            <br/>
             <label>
-                Nome ou E-mail:
+                Nome ou E-mail:<br/>
                 <input type="text" name="nome" placeholder="Digite seu nome ou e-mail" required/>
             </label>
+            <br/>
+            <br/>
             <label >
-                Senha:
+                Senha:<br/>
                 <input type="password" name="senha" placeholder="Digite sua senha" required/>
             </label>
+            <br/>
+            <br/>
             <input type="submit" value="Enviar"/>
         </form>
     );
