beb883c7ae37da06c5d272efae102bd9d29cf9f0
luna-noronha <luna.noronha@example.com>
2025-01-24 20:40:41 -0300

    feat (chore): Alterações para a junção front-end e back-end. Foi adicionado novos arquivos e apagados outros para a melhor interação entre o front e o back, adição de novas rotas, aumento de funcionalidades dentro de arquivos, etc.

--- projeto-rjm/src/Routers.jsx
+++ projeto-rjm/src/Routers.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import Login from "./components/Login";
 import Registro from "./components/Registro";
 import RecSenha from "./components/Rec";
 import App from './components/App';
 import Principal from "./components/Principal";
+import Get from "./API/User";
 
-function Routers(){
+const Routers = ()=>{
     return(
-        
         <Router>
             <Routes>
                 <Route path="/" element={<App/>}/>
@@ -15,6 +16,7 @@ function Routers(){
                 <Route path="/registro" element={<Registro/>}/>
                 <Route path="login/recSenha" element={<RecSenha/>}/>
                 <Route path="/principal" element={<Principal/>}/>
+                <Route path="/usuarios" element={<Get/>}/>
             </Routes>
         </Router>   
     )
--- projeto-rjm/src/components/Registro/index.jsx
+++ projeto-rjm/src/components/Registro/index.jsx
@@ -1,33 +1,75 @@
 // Deve ser melhorado o estilo e colocado o caminho para envio das informações
+import { useEffect, useState } from 'react'
 import styles from './Registro.module.css'
+import axios from "axios";
 
 const Registro = ()=>{
+    // caminho para da API
+    const API_URL = "http://localhost:3000/api/v2/usuarios"
+    // armazena as informações do forms
+    const [nome, setNome] = useState('')
+    const [senha, setSenha] = useState('')
+    const [email, setEmail] = useState('')
+    const [user_git, setNg] = useState('')
+
+    function onSave(e){
+        e.preventDefault()
+        postAPIData({nome, email, senha, user_git})
+        // Limpa as variáveis
+        setNome('')
+        setSenha('')
+        setEmail('')
+        setNg('')
+        // joga para o login
+        window.location.href="login"
+    }
+    // função de post
+    function postAPIData( param ) {
+        return axios.post(API_URL, param)
+    }
+    // forms
     return (
         <div className={styles.registro}>
             <center>
                 <h1>
                     Faça seu Cadastro!
                 </h1>
-                <form action="" method="POST">
+                <form onSubmit={onSave}>
                     <label>
                         Nome:<br/>
-                        <input type="text" name="nome" placeholder="Digite seu nome" required/>
+                        <input 
+                            type="text" name="nome" 
+                            placeholder="Digite seu nome" required
+                            onChange={(e) => setNome(e.target.value)}
+                        />
                     </label>
                     <br/>
                     <label >
                         Senha:
                         <br/>
-                        <input type="password" name="senha" placeholder="Digite sua senha" required/>
+                        <input 
+                            type="password" name="senha" 
+                            placeholder="Digite sua senha" required
+                            onChange={(e) => setSenha(e.target.value)}    
+                        />
                     </label>
                     <br/>
                     <label>
                         E-mail:<br/>
-                        <input type="mail" name="email" placeholder="Digite seu e-mail" required/>
+                        <input 
+                            type="mail" name="email" 
+                            placeholder="Digite seu e-mail" required
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </label>
                     <br/>
                     <label>
                     Nome no Github:<br/>
-                    <input type="text" name="n_g" placeholder="Digite seu nome no github" required/>
+                    <input 
+                        type="text" name="ng" 
+                        placeholder="Digite seu nome no github" required
+                        onChange={(e) => setNg(e.target.value)}
+                    />
                     </label>
                     <br/>
                     <button type="submit">Enviar</button>
